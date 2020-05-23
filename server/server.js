616f3c5ef19e228cf07d87af56fb0674dc55955a
Mohamed Haneef <mhaneef50673@gmail.com>
2020-05-23 20:44:47 +0400

    Add: updated server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
-console.log('publicPath', publicPath);
-const port = process.env.PORT || 3000;
+console.log('publicPath',publicPath);
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
-app.listen(port, () => {
-   console.log('Server is up!');
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+app.listen(process.env.PORT || 3000);
